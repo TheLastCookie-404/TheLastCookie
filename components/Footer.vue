aad90eea7b51d10a83f9f201b19b0695b7e88c82
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-05-02 21:11:07 +0500

    Footer updated

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,12 @@
   <footer class="footer sm:footer-horizontal bg-base-200 text-neutral-content p-10">
     <aside>
       <p>
-        <span class="text-lg">By: TheLastCookie-404</span>
+        <span class="text-lg">
+          3D model by:
+          <a class="link" href="https://sketchfab.com/514OnKoishi">LunaR</a>
+        </span>
         <br />
-        Made for fun, to test 3d models
+        This site is for fun, by TheLastCookie-404
       </p>
     </aside>
   </footer>
--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="navbar bg-base-100 shadow-sm">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl">TheLastCookie-404</a>
+      <a class="btn btn-ghost text-xl">TheLastCookie</a>
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1">
-        <li><a href="https://github.com/TheLastCookie-404">Github</a></li>
+        <li>
+          <a class="link" href="https://github.com/TheLastCookie-404">Github</a>
+        </li>
       </ul>
     </div>
   </div>
