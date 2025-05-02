54f61908289a7d2a4db2a4031fbbd307509f5e50
TheLastCookie-404 <se660745@gmail.com>
2025-05-02 17:04:08 +0000

    Footer rolled back

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,12 +2,9 @@
   <footer class="footer sm:footer-horizontal bg-base-200 text-neutral-content p-10">
     <aside>
       <p>
-        <span class="text-lg">
-          3D model by:
-          <a class="link" href="https://sketchfab.com/514OnKoishi">LunaR</a>
-        </span>
+        <span class="text-lg">By: TheLastCookie-404</span>
         <br />
-        This site is for fun, by TheLastCookie-404
+        Made for fun, to test 3d models
       </p>
     </aside>
   </footer>
