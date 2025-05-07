299b90e82a1cf69cc9a00e473de4c132e85a61a9
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-05-07 17:45:51 +0500

    Cookie model added

---
+++ components/Cookie.vue
@@ -0,0 +1,13 @@
+<template>
+  <primitive :object="model" />
+</template>
+
+<script setup lang="ts">
+  import { useGLTF } from "@tresjs/cientos";
+
+  const { scene: model } = await useGLTF("/models/cookie/CookieBite.glb");
+
+  model.traverse((child) => {
+    if (child.isObject3D) child.castShadow = true;
+  });
+</script>
--- pages/Model.vue
+++ pages/Model.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="h-[80vh]">
     <TresCanvas preset="realistic" :tone-mapping-exposure="1">
-      <TresPerspectiveCamera :position="[0, 2, 3]" :look-at="[0, 0, 0]" />
+      <TresPerspectiveCamera :position="[-0.6, 1.3, 3.5]" :look-at="[0, 0, 0]" />
       <OrbitControls />
       <Suspense>
         <Turtle :position="[0, -0.2, 0]" />
+      </Suspense>
+      <Suspense>
+        <Cookie :position="[0, -0.2, 1.5]" :scale="0.25" :rotate-y="3" />
       </Suspense>
       <TresMesh :position="[0, -0.44, 0]" receive-shadow>
         <TresBoxGeometry :args="[20, 0.5, 20]" />
@@ -17,6 +20,8 @@
 </template>
 
 <script setup lang="ts">
+  import Cookie from "~/components/Cookie.vue";
+
   definePageMeta({
     layout: "model",
   });
